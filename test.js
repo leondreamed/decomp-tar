6a5f1aef31d35808a893e314bdece1b6d4a77a51
Leon Si <leon@leondreamed.com>
2023-09-30 17:05:50 -0400

    fix: tests

--- test.js
+++ test.js
@@ -1,46 +1,51 @@
-import fs from 'fs';
-import path from 'path';
-import isJpg from 'is-jpg';
-import pify from 'pify';
-import test from 'ava';
-import m from '.';
+import fs from "fs";
+import isJpg from "is-jpg";
+import test from "ava";
+import m from "./index.js";
+import { join, filename } from "desm";
 
-const fsP = pify(fs);
+test("extract file", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixtures", "file.tar")
+  );
+  const files = await m()(buf);
 
-test('extract file', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'file.tar'));
-	const files = await m()(buf);
-
-	t.is(files[0].path, 'test.jpg');
-	t.true(isJpg(files[0].data));
+  t.is(files[0].path, "test.jpg");
+  t.true(isJpg(files[0].data));
 });
 
-test('extract file using streams', async t => {
-	const stream = fs.createReadStream(path.join(__dirname, 'fixtures', 'file.tar'));
-	const files = await m()(stream);
+test("extract file using streams", async (t) => {
+  const stream = fs.createReadStream(
+    join(import.meta.url, "fixtures", "file.tar")
+  );
+  const files = await m()(stream);
 
-	t.is(files[0].path, 'test.jpg');
-	t.true(isJpg(files[0].data));
+  t.is(files[0].path, "test.jpg");
+  t.true(isJpg(files[0].data));
 });
 
-test('extract symlinks', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'symlink.tar'));
-	const files = await m()(buf);
+test("extract symlinks", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixtures", "symlink.tar")
+  );
+  const files = await m()(buf);
 
-	t.is(files[0].path, 'test-symlink/symlink');
-	t.is(files[0].type, 'symlink');
-	t.is(files[0].linkname, 'file.txt');
-	t.is(files[1].path, 'test-symlink/file.txt');
-	t.is(files[1].type, 'file');
+  t.is(files[0].path, "test-symlink/symlink");
+  t.is(files[0].type, "symlink");
+  t.is(files[0].linkname, "file.txt");
+  t.is(files[1].path, "test-symlink/file.txt");
+  t.is(files[1].type, "file");
 });
 
-test('return empty array if non-valid file is supplied', async t => {
-	const buf = await fsP.readFile(__filename);
-	const files = await m()(buf);
+test("return empty array if non-valid file is supplied", async (t) => {
+  const buf = await fs.promises.readFile(filename(import.meta.url));
+  const files = await m()(buf);
 
-	t.is(files.length, 0);
+  t.is(files.length, 0);
 });
 
-test('throw on wrong input', async t => {
-	await t.throws(m()('foo'), 'Expected a Buffer or Stream, got string');
+test("throw on wrong input", async (t) => {
+  await t.throwsAsync(() => m()("foo"), {
+    message: "Expected a Buffer or Stream, got string",
+  });
 });
